32761f28e1029e422f7adda894317adf1b49f292
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-06-02 20:31:58 -0300

    refactor: improve enrollment bills creation

--- lib/queropay_web/services/enrollment_bills_creation.ex
+++ lib/queropay_web/services/enrollment_bills_creation.ex
@@ -9,44 +9,40 @@ defmodule Queropay.EnrollmentBillsCreation do
 
   def perform(%Enrollment{} = enrollment) do
     params = %{
-      "value" => calculate_bill_value(enrollment),
+      "value" => enrollment.full_value / enrollment.amount_bills,
       "status" => "aberta",
       "enrollment_id" => enrollment.id
     }
 
-    date = first_due_date(enrollment)
-
-    for index <- 0..(enrollment.amount_bills - 1) do
-      d = Timex.shift(date, months: index)
-      params = Map.put(params, "due_date", due_date(enrollment.due_day, d.month, d.year))
-      params |> create_bill()
-    end
+    generate_bills(enrollment, params)
   end
 
   def perform(_), do: %{error: %{message: "Parâmetros inválidos!"}}
 
-  def calculate_bill_value(enrollment) do
-    enrollment.full_value / enrollment.amount_bills
+  def generate_bills(enrollment, params) do
+    first_due_date = get_first_due_date(enrollment)
+
+    for index <- 0..(enrollment.amount_bills - 1) do
+      due_date = Timex.shift(first_due_date, months: index)
+
+      Map.put(params, "due_date", build_date(enrollment.due_day, due_date.month, due_date.year))
+        |> Bill.changeset()
+        |> Repo.insert()
+    end
   end
 
-  def first_due_date(enrollment) do
+  def get_first_due_date(enrollment) do
     today = Timex.today()
 
     first_month = if(today.day >= enrollment.due_day, do: today.month + 1, else: today.month)
 
-    due_date(enrollment.due_day, first_month, today.year)
+    build_date(enrollment.due_day, first_month, today.year)
   end
 
-  def due_date(day, month, year) do
+  def build_date(day, month, year) do
     case Date.new(year, month, day) do
       {:ok, date} -> date
-      {:error, :invalid_date} -> Date.end_of_month(Date.new!(year, month, 1))
+      {:error, :invalid_date} -> Timex.end_of_month(year, month)
     end
-  end
-
-  def create_bill(%{} = params) do
-    params
-    |> Bill.changeset()
-    |> Repo.insert()
   end
 end
