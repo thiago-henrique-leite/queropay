91563464924de0eefad47e091ee5cccd0b4b9496
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-06-02 00:03:34 -0300

    feat: add bill validation

--- lib/queropay_web/models/bill.ex
+++ lib/queropay_web/models/bill.ex
@@ -38,6 +38,7 @@ defmodule Queropay.Bill do
     |> cast(params, @fields_that_can_be_changed)
     |> validate_required(@required_fields)
     |> validate_number(:value, greater_than: 0)
+    |> validate_inclusion(:status, ["aberta", "pendente", "paga", "cancelada"])
     |> foreign_key_constraint(:enrollment, name: :bills_enrollment_id_fkey)
   end
 end
